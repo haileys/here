97d72b9271fd932575c30ffd149b2e9c815892ae
charliesome <charlie@charliesomerville.com>
2011-06-27 23:01:00 +1000

    fixed time module

--- modules/time/time.c
+++ modules/time/time.c
@@ -4,9 +4,11 @@
 #include "module.h"
 
 int here_dispatch(server_t* server, client_t* client, http_request_header_t* headers, char* real_path, char* extension) {
-    if(strcmp("/time", headers->uri)) {
+    if(strcmp("/time", headers->uri) == 0) {
         char buff[1000];
-        sprintf(buff, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nThe current UNIX timestamp is: <b>%d</b>", (int)time(NULL));
+        char body[100];
+        sprintf(body, "The current UNIX timestamp is: <b>%d</b>", (int)time(NULL));
+        sprintf(buff, "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/html\r\nContent-Length: %d\r\n\r\n%s", (int)strlen(body), body);
         send(client->sockfd, buff, strlen(buff), 0);
         return 1;
     }
